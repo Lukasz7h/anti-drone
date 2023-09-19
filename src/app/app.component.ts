ca521fdb026beea447629f755079a1d7781a5595
Lukasz <luuukasz368@gmail.com>
2023-09-19 21:53:25 +0200

    anti-drone antennas page start
    1. App was created by angular framework v15.2.9 Images and layout have been got from figma app. To facilitate creating process of style bootstrap library is include.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+
+export class AppComponent implements AfterViewInit {
+
   title = 'anti-drone';
+
+  @ViewChild("nav_list")
+  navList: ElementRef | undefined;
+
+  @ViewChild("bar_menu")
+  barMenu: ElementRef | undefined;
+
+  ngAfterViewInit(): void {
+
+    function showList(e: MouseEvent)
+    {
+      
+    };
+
+    this.barMenu?.nativeElement
+    .addEventListener("click", showList);
+  }
 }
